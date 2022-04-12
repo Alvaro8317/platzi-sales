ffe94e8d571e5a3fbe7507f483b3a732450f0ba6
Alvaro8317 <eduardo831_@hotmail.com>
2022-04-11 22:08:29 -0500

    I added the other exercice with *args and *kwargs of the course of Pildoras informaticas <3

--- decorators.py
+++ decorators.py
@@ -35,20 +35,28 @@ if __name__ == '__main__':
 """
 
 def func_decorator(function_parameter): #Se crea función decoradora que recibe otra función
-    def interior_function(): #Wrapper también llamada función C
+    def interior_function(*args, **kwargs): # Con el *args se indica que puede recibir x parametros # kwargs son para parametros llave vs
         #Acciones adicionales que decoran
         print("I\'ll perform a calculation: ")
-        function_parameter()
+        function_parameter(*args, **kwargs) #Se debe de indicar el args aquí también
         print("I did the calculation")
     return interior_function #Se devuelve siempre la función interior
 
 @func_decorator #Se escribe esta sintaxis antes de la función que se quiere decorar
-def addition():
-    print(15+15)
+def addition(a,b,c):
+    print(a+b+c)
 
 @func_decorator
-def subtraction():
-    print(15+15)
-    
-addition()
-subtraction()
+def subtraction(a,b,c):
+    print(a-b-c)
+
+@func_decorator
+def squaring(base,exponent):
+    print(pow(base,exponent))
+
+a = int(input("First number: "))
+b = int(input("Second number: "))
+c = int(input("Third number: "))
+addition(a,b,c)
+subtraction(a,b,c)
+squaring(base=a,exponent=b)
